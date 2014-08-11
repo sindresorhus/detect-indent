f87d01a9576768620ee46c10574dba0479d2a39c
jonschlinkert <dev@sellside.com>
2014-08-10 21:45:34 -0400

    use unicode spaces in tests. also change last test to expect `0` instead of `null` per pending changes.

--- test.js
+++ test.js
@@ -9,7 +9,7 @@ function getFile(file) {
 }
 
 it('should detect the indent of a file with space indent', function () {
-	assert.equal(detectIndent(getFile('fixture/space.js')), '    ');
+	assert.equal(detectIndent(getFile('fixture/space.js')), '\u0020\u0020\u0020\u0020');
 });
 
 it('should detect the indent of a file with tab indent', function () {
@@ -17,17 +17,17 @@ it('should detect the indent of a file with tab indent', function () {
 });
 
 it('should detect the indent of a file with mostly tabs', function () {
-	assert.equal(detectIndent(getFile('fixture/mixed-tab.js')), '\t');
+	assert.equal(detectIndent(getFile('fixture/mixed-tab.js')), '\u0020\u0020');
 });
 
 it('should detect the indent of a file with mostly spaces', function () {
-	assert.equal(detectIndent(getFile('fixture/mixed-space.js')), '    ');
+	assert.equal(detectIndent(getFile('fixture/mixed-space.js')), '\u0020\u0020\u0020\u0020');
 });
 
 it('should detect the indent of a weirdly indented vendor prefixed CSS', function () {
-	assert.equal(detectIndent(getFile('fixture/vendor-prefixed-css.css')), '    ');
+	assert.equal(detectIndent(getFile('fixture/vendor-prefixed-css.css')), '\u0020\u0020\u0020\u0020');
 });
 
 it('should return `null` when there are no indentation', function () {
-	assert.equal(detectIndent('<ul></ul>'), null);
+	assert.equal(detectIndent('<ul></ul>'), 0);
 });
